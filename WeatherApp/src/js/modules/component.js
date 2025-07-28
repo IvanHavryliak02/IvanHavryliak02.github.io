840f13c6e88b57fb660140723fc7802a703ef3ff
Ivan <ivan.webdevelp@gmail.com>
2025-07-28 17:42:05 +0200

    Iproved style parser. Now it's supports pseudo- classes/elements and media queries

--- WeatherApp/src/js/modules/component.js
+++ WeatherApp/src/js/modules/component.js
@@ -25,46 +25,51 @@ export default class Component{
     }
 
     applyStyles(){
-        if(this.styles){
-            let newRule = `.${this.elementClassName}{\n`;
-            for(let styleProp in this.styles){
-                if(typeof this.styles[styleProp] !== 'object'){
-                    const cssProp = styleProp.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
-                    const value = this.styles[styleProp];
-                    newRule += `\t${cssProp}: ${value};\n`;
-                }
-            }
-            newRule += '}'
-            this.styleSheet.insertRule(newRule, this.styleSheet.length);
+        const cssSheet = this.styleSheet;
+        const selector = this.elementClassName;
+        const styles = this.styles;
+
+        createNewRule(styles);
+
+        function convCamelToKebab(camelExp){ 
+            return camelExp.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
         }
 
-        const pseudoClasses = this.styles.pseudoClasses;
-        if(pseudoClasses){
-            for(let psClassObjName in pseudoClasses){
-                const psClass = psClassObjName.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
-                let newRule = `.${this.elementClassName}:${psClass}{\n`
-                for (let styleProp in pseudoClasses[psClassObjName]){
-                    const cssProp = styleProp.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
-                    const value = pseudoClasses[psClassObjName][styleProp];
-                    newRule += `\t${cssProp}: ${value};\n`;
+        function createNewRule(obj, pseudo = '', media = ''){
+            const fullSelector = `.${selector}${pseudo}`;
+            let ruleBody = createCssProps(obj);
+            
+            if(ruleBody){
+                let rule = `${fullSelector} {\n${ruleBody}}\n`
+                if(media) {
+                    rule = `${media} {\n${rule}}\n`
                 }
-                newRule += '}'
-                this.styleSheet.insertRule(newRule, this.styleSheet.length);
+                cssSheet.insertRule(rule, cssSheet.cssRules.length);
+            }
+
+            for(let prop in obj){
+                const value = obj[prop];
+
+                if(typeof value === 'object'){
+                    if(/^:/.test(prop)){
+                        createNewRule(value, `${pseudo}${convCamelToKebab(prop)}`, media);
+                    }
+                    else if(/^@media/.test(prop)){
+                        createNewRule(value, pseudo, prop);
+                    }
+                }
             }
         }
 
-        const pseudoElements = this.styles.pseudoElements;
-        if(pseudoElements){
-            for(let psElementName in pseudoElements){
-                const psElement = psElementName.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
-                let newRule = `.${this.elementClassName}::${psElement}{\n`
-                for (let styleProp in pseudoElements[psElementName]){
-                    const cssProp = styleProp.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
-                    const value = pseudoElements[psElementName][styleProp];
-                    newRule += `\t${cssProp}: ${value};\n`;
+        function createCssProps(obj){
+            let props = '';
+            if(obj){
+                for(let prop in obj){
+                    if(typeof obj[prop] !== 'object'){
+                        props += `\t${convCamelToKebab(prop)}: ${obj[prop]};\n`;
+                    }
                 }
-                newRule += '}'
-                this.styleSheet.insertRule(newRule, this.styleSheet.length);
+                return props;
             }
         }
     }
